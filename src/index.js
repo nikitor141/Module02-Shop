10919a3d860b027c259f667f7f94ca726cc333cc
nikitor141 <nikitor141@gmail.com>
2023-03-23 23:58:20 +0300

    edit header

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import './styles/main.scss';
 
-
+document.querySelector('.burger').addEventListener('click', function () {
+   let elmsToActive = [this.querySelector('.burger__body'), document.body, document.querySelector('.header__menu-list')];
+   elmsToActive.forEach(item => {
+      item.classList.toggle('active');
+   })
+   if (document.querySelector('.header__menu-list').classList.contains('active')) {
+      document.querySelector('.header__menu').addEventListener('click', e => {
+         if (e.target.closest('.header__menu-link')) {
+            elmsToActive.forEach(item => {
+               item.classList.remove('active');
+            })
+         }
+      })
+   }
+})
